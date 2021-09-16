c11d88aa41014bf5c3e6e6acc167580198e1ed13
William <williambecker321@gmail.com>
2021-09-16 12:03:44 +0200

    First commit Started Prodjekt calc

---
+++ Exersise 1/ACME Calc .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exersise_1
+{
+    public partial class ACME_Calc : Form
+    {
+        public ACME_Calc()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
